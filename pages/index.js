303a253f2fcc4e75a6d60cb1c45412c8158c9b35
wittayasutt <wittayasutt@gmail.com>
2021-02-22 23:29:57 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Crowdcheck</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main className={styles.main}>
+				<h1 className={styles.title}>Crowdcheck.info</h1>
+			</main>
+
+			<footer className={styles.footer}>Footer</footer>
+		</div>
+	);
+}
