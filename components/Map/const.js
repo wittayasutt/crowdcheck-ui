0ee70c616ccd2e1495637c09d464bffd77234b63
wittayasutt <wittayasutt@gmail.com>
2021-12-03 22:19:49 +0700

    update default center

--- components/Map/const.js
+++ components/Map/const.js
@@ -1,9 +1,9 @@
-const CENTER_OF_BANGKOK_LAT = 13.73017;
-const CENTER_OF_BANGKOK_LNG = 100.5132;
+const CENTER_OF_BANGKOK_LAT = 18.78853382529468;
+const CENTER_OF_BANGKOK_LNG = 98.98566699754699;
 
 export const defaultCenter = {
 	lat: CENTER_OF_BANGKOK_LAT,
 	lng: CENTER_OF_BANGKOK_LNG,
 };
 
-export const defaultZoom = 16;
+export const defaultZoom = 15;
